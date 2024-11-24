aa60567e77d8b5c66704bf8e29faa3a33729decf
Isaque Almeida <isaque.santos@ufpe.br>
2024-11-24 17:22:33 -0300

    Adicionado o método remover_colunas, normalizar_dados, separar_treino_teste e preparar_dados

--- prevendo_atraso.py
+++ prevendo_atraso.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
 
 
 class DataPrep:
@@ -13,7 +14,7 @@ class DataPrep:
         df.drop(columns=variavel, inplace=True)
         return df
 
-    def tratar_variaveis_categoricas(self) -> None:
+    def _tratar_variaveis_categoricas(self) -> None:
         # Usar One Hot Encoder para tratar as variáveis categóricas
         self.data = DataPrep._one_hot_encoder(self.data, 'Warehouse_block')
 
@@ -23,5 +24,31 @@ class DataPrep:
 
         self.data = DataPrep._one_hot_encoder(self.data, 'Gender')
     
-    def remover_colunas(self) -> None:
-        pass
+    def _remover_colunas(self) -> None:
+        # Remove as variáveis que não serão utilizadas pelo modelo
+        self.data.drop(columns='ID', inplace=True)
+    
+    def _normalizar_dados(self) -> None:
+        variaveis = self.data.drop(columns='Reached.on.Time_y.N')
+        var_cols = variaveis.columns
+        resposta = self.data['Discount_offered']
+
+        scaler = MinMaxScaler()
+        variaveis = scaler.fit_transform(variaveis)
+        variaveis = pd.DataFrame(variaveis, columns=var_cols)
+
+        self.data = pd.concat([variaveis, resposta], axis=1)
+    
+    def _separar_treino_teste(self):
+        # Divide o dataset em conjunto de treino e teste
+        treino, teste = train_test_split(self.data, test_size=0.3, random_state=2024)
+        return treino, teste
+
+    def preparar_dados(self):
+        # Aplica todas as transformações de dados
+        self.tratar_variaveis_categoricas()
+        self._remover_colunas()
+        self._normalizar_dados()
+        treino, teste = self._separar_treino_teste()
+
+        return treino, teste
